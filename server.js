4430d101c8ec744bb883d1263c7c15502e6ebe2c
Rodion <www.rodionkochetkow@gmail.com>
2021-02-16 13:03:38 +0300

    create node static server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname));
+
+app.get("/build", (req, res) => {
+  res.render("index.html");
+})
+
+app.listen(port, () => {
+  console.log("started listening")
+})
